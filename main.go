560ad7bf36c206e9e3a6dc0015cc44c4e22afcdb
zzwsec <zzwsec@163.com>
2026-02-12 13:53:10 +0800

    add getLinesChannel

--- main.go
+++ main.go
@@ -1,27 +1,57 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
 	f, err := os.Open("messages.txt")
 	if err != nil {
-		fmt.Println(err.Error())
+		log.Fatalf("could not open messages.txt: %s\n", err)
 		return
 	}
-	dat := make([]byte, 8)
-	for {
-		n, err := f.Read(dat)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalln(err)
-		}
-		fmt.Printf("read: %s\n", string(dat[:n]))
+	linesChan := getLinesChannel(f)
+	for line := range linesChan {
+		fmt.Println("read: ", line)
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer func() {
+			err := f.Close()
+			fmt.Println(err)
+		}()
+		defer close(lines)
+		currentLine := ""
+
+		for {
+			dat := make([]byte, 8)
+			n, err := f.Read(dat)
+			if err != nil {
+				if currentLine != "" {
+					lines <- currentLine
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				break
+			}
+
+			parts := strings.Split(string(dat[:n]), "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- fmt.Sprintf("%s%s", currentLine, parts[i])
+				currentLine = ""
+			}
+			currentLine += parts[len(parts)-1]
+		}
+	}()
+	return lines
 }
