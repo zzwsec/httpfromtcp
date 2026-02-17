a16a485804ec03a8287c099c47b7d7ec1046041c
zzwsec <zzwsec@163.com>
2026-02-17 19:54:20 +0800

    add parse request

--- internal/request/request.go
+++ internal/request/request.go
@@ -8,7 +8,15 @@ import (
 
 type Request struct {
 	RequestLine RequestLine
+	RequestStat RequestStat
 }
+
+type RequestStat int
+
+const (
+	initialized RequestStat = iota
+	done
+)
 
 type RequestLine struct {
 	HttpVersion   string
@@ -17,27 +25,50 @@ type RequestLine struct {
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	dat, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
-	}
-	requestLine, err := parseRequestLine(dat)
-	if err != nil {
-		return nil, err
-	}
+	req := &Request{RequestStat: initialized}
+	buffer := make([]byte, 0, 8) // 主缓冲区
+	tmp := make([]byte, 8)       // 临时缓冲区
 
-	return &Request{
-		RequestLine: requestLine,
-	}, nil
+	for req.RequestStat != done {
+		n, err := reader.Read(tmp)
+		if n > 0 {
+			// 1. 数据进入：追加到主缓冲区
+			buffer = append(buffer, tmp[:n]...)
+
+			// 2. 尝试解析：消耗主缓冲区的数据
+			consumed, err := req.parse(buffer)
+			if err != nil {
+				return nil, err
+			}
+
+			// 3. 数据移出：删掉已经消耗的部分（进出移动）
+			if consumed > 0 {
+				buffer = buffer[consumed:]
+			}
+		}
+
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return nil, err
+		}
+	}
+	return req, nil
 }
 
-func parseRequestLine(dat []byte) (RequestLine, error) {
+func parseRequestLine(dat []byte, rl *RequestLine) (int, error) {
 	s := string(dat)
+	exists := strings.Contains(s, "\r\n")
+	if !exists {
+		return 0, nil
+	}
+
 	result := strings.Split(s, "\r\n")[0]
 	parts := strings.Fields(result)
 
 	if len(parts) != 3 {
-		return RequestLine{}, errors.New("invalid request line: wrong number of parts")
+		return 0, errors.New("invalid request line: wrong number of parts")
 	}
 
 	method := parts[0]
@@ -45,21 +76,42 @@ func parseRequestLine(dat []byte) (RequestLine, error) {
 	httpVersion := parts[2]
 
 	if method != strings.ToUpper(method) || len(method) == 0 {
-		return RequestLine{}, errors.New("invalid method")
+		return 0, errors.New("invalid method")
+	}
+
+	if !strings.HasPrefix(target, "/") {
+		return 0, errors.New("invalid http target format")
 	}
 
 	if !strings.HasPrefix(httpVersion, "HTTP/") {
-		return RequestLine{}, errors.New("invalid http version format")
+		return 0, errors.New("invalid http version format")
 	}
 
 	version := strings.TrimPrefix(httpVersion, "HTTP/")
 	if version != "1.1" {
-		return RequestLine{}, errors.New("unsupported http version")
+		return 0, errors.New("unsupported http version")
 	}
+	rl.Method = method
+	rl.RequestTarget = target
+	rl.HttpVersion = httpVersion
 
-	return RequestLine{
-		Method:        method,
-		RequestTarget: target,
-		HttpVersion:   version,
-	}, nil
+	return len(result + "\r\n"), nil
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+	switch r.RequestStat {
+	case initialized:
+		n, err := parseRequestLine(data, &r.RequestLine)
+		if err != nil {
+			return 0, err
+		}
+		if n > 0 {
+			r.RequestStat = done
+		}
+		return 0, nil
+	case done:
+		return 0, nil
+	default:
+		return 0, errors.New("unknown stat")
+	}
 }
---
+++ internal/request/request_test.go
@@ -0,0 +1,23 @@
+package request
+
+import "io"
+
+type chunkReader struct {
+	data            string
+	numBytesPerRead int
+	pos             int
+}
+
+func (cr *chunkReader) Read(p []byte) (n int, err error) {
+	if cr.pos >= len(cr.data) {
+		return 0, io.EOF
+	}
+	endIndex := cr.pos + cr.numBytesPerRead
+	if endIndex > len(cr.data) {
+		endIndex = len(cr.data)
+	}
+	n = copy(p, cr.data[cr.pos:endIndex])
+	cr.pos += n
+
+	return n, nil
+}
